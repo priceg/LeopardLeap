3a556726291d347c7802866a1c247efa5c74b320
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-07-10 16:33:21 -0400

    Main Page
    basic loook for main page done

--- app/src/main/java/com/example/leopardleap/Visitor_Landing.java
+++ app/src/main/java/com/example/leopardleap/Visitor_Landing.java
@@ -3,12 +3,11 @@ package com.example.leopardleap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Visitor_Landing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_visitor__landing);
     }
 }
