72a0cf05cdc9490d4357109e5c4c189881160cfc
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-07-17 23:43:43 -0400

    tons of stuff
    all ui stuff, nfc will be this weekend because we need to get poster done

--- app/src/main/java/com/example/leopardleap/TourGuide_Roadmap.java
+++ app/src/main/java/com/example/leopardleap/TourGuide_Roadmap.java
@@ -1,13 +1,121 @@
 package com.example.leopardleap;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.view.ViewGroup.LayoutParams;
+
+import java.lang.invoke.ConstantCallSite;
 
 public class TourGuide_Roadmap extends AppCompatActivity {
+
+    static String TAG = "MyApp";
+
+    private Context mContext;
+    private Activity mActivity;
+
+    private ConstraintLayout mConstraintLayout;
+    private Button mButton;
+
+    private PopupWindow mPopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_guide__roadmap);
+
+        // Get the application context
+        mContext = getApplicationContext();
+
+        // Get the activity
+        mActivity = TourGuide_Roadmap.this;
+
+        // Get the widgets reference from XML layout
+        mConstraintLayout = (ConstraintLayout) findViewById(R.id.rm_layout);
+        mButton = (Button) findViewById(R.id.button1);
+
+        Log.v(TAG, "Button1 is clicked");
+
+        // Set a click listener for the text view
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initialize a new instance of LayoutInflater service
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                Log.v(TAG, "Layout is inflated");
+                // Inflate the custom layout/view
+                View customView = inflater.inflate(R.layout.custom_layout,null);
+
+                /*
+                    public PopupWindow (View contentView, int width, int height)
+                        Create a new non focusable popup window which can display the contentView.
+                        The dimension of the window must be passed to this constructor.
+
+                        The popup does not provide any background. This should be handled by
+                        the content view.
+
+                    Parameters
+                        contentView : the popup's content
+                        width : the popup's width
+                        height : the popup's height
+                */
+                // Initialize a new instance of popup window
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT
+                );
+
+                // Set an elevation value for popup window
+                // Call requires API level 21
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                    Log.v(TAG, "Elevation set");
+                }
+
+
+                // Get a reference for the custom view close button
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.close);
+
+                // Set a click listener for the popup window close button
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Dismiss the popup window
+                        mPopupWindow.dismiss();
+                        Log.v(TAG, "Window closed");
+                    }
+                });
+
+                /*
+                    public void showAtLocation (View parent, int gravity, int x, int y)
+                        Display the content view in a popup window at the specified location. If the
+                        popup window cannot fit on screen, it will be clipped.
+                        Learn WindowManager.LayoutParams for more information on how gravity and the x
+                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
+                        to specifying Gravity.LEFT | Gravity.TOP.
+
+                    Parameters
+                        parent : a parent view to get the getWindowToken() token from
+                        gravity : the gravity which controls the placement of the popup window
+                        x : the popup's x location offset
+                        y : the popup's y location offset
+                */
+                // Finally, show the popup window at the center location of root relative layout
+                mPopupWindow.showAtLocation(mConstraintLayout, Gravity.CENTER,0,0);
+                Log.v(TAG, "It's here it's here");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/leopardleap/Visitor_Roadmap.java
+++ app/src/main/java/com/example/leopardleap/Visitor_Roadmap.java
@@ -1,13 +1,28 @@
 package com.example.leopardleap;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class Visitor_Roadmap extends AppCompatActivity {
+
+    private Context mContext;
+    private Activity mActivity;
+
+    private RelativeLayout mRelativeLayout;
+    private Button mButton;
+
+    private PopupWindow mPopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visitor__roadmap);
+
+
     }
 }
