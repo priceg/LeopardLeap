cb3b3ddbc52473fa6050c9b6022dabe68b18e22d
aconne22 <46835077+aconne22@users.noreply.github.com>
2019-07-23 14:32:35 -0400

    nfc read complete
    NFC Tags can now be read, contents stored in string "message"

--- app/src/main/java/com/example/leopardleap/TourGuide_Roadmap.java
+++ app/src/main/java/com/example/leopardleap/TourGuide_Roadmap.java
@@ -5,9 +5,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Build;
 import android.os.Parcelable;
 import android.support.constraint.ConstraintLayout;
@@ -28,17 +31,15 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.invoke.ConstantCallSite;
 
-public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClickListener{
+public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClickListener {
 
     static String TAG = "MyApp";
 
-    private Context mContext;
-    private Activity mActivity;
-
-    private ConstraintLayout mConstraintLayout;
+    //Buttons for checkpoints along Roadmap
     private Button tansey;
     private Button wattson;
     private Button beatty;
@@ -46,34 +47,23 @@ public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClick
     private Button baker;
     private Button huntington;
 
-    TextView tvNFCContent;
-    NfcAdapter nfcAdapter;
-    PendingIntent pendingIntent;
-    IntentFilter writeTagFilters[];
-
-    Tag myTag;
-
+    //Popup Window Related
     private PopupWindow mPopupWindow;
+    private Context mContext;
+    private Activity mActivity;
+    private ConstraintLayout mConstraintLayout;
+
+    //NFC Related
+    private NfcAdapter mNfcAdapter;
+    private TextView nfcMessage;
+
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_guide__roadmap);
-
-        tvNFCContent = (TextView) findViewById(R.id.nfc_contents);
-
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-        if (nfcAdapter == null) {
-            // Stop here, we definitely need NFC
-            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
-            finish();
-        }
-        readFromIntent(getIntent());
-
-        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
-        writeTagFilters = new IntentFilter[] { tagDetected };
+        initNFC();
 
         Button end_btn = (Button) findViewById(R.id.end_btn);
         end_btn.setOnClickListener(new View.OnClickListener() {
@@ -85,13 +75,11 @@ public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClick
             }
         });
 
-        // Get the application context
-        mContext = getApplicationContext();
 
-        // Get the activity
+        mContext = getApplicationContext();
         mActivity = TourGuide_Roadmap.this;
 
-        // Get the widgets reference from XML layout
+        //Necessary XML widgets (layout + buttons)
         mConstraintLayout = (ConstraintLayout) findViewById(R.id.rm_layout);
         tansey = (Button) findViewById(R.id.button1);
         wattson = (Button) findViewById(R.id.button2);
@@ -102,7 +90,7 @@ public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClick
 
         Log.v(TAG, "Button1 is clicked");
 
-        // Set a click listener for the text view
+        //Series of click listeners for each button, calls default onClick which contains switch
         tansey.setOnClickListener(this);
         wattson.setOnClickListener(this);
         beatty.setOnClickListener(this);
@@ -114,34 +102,33 @@ public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClick
 
     @Override
     public void onClick(View v) {
-        // Initialize a new instance of LayoutInflater service
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
         Log.v(TAG, "Layout is inflated");
         // Inflate the custom layout/view
-        View customView = inflater.inflate(R.layout.custom_layout,null);
+        View customView = inflater.inflate(R.layout.custom_layout, null);
 
-        // Initialize a new instance of popup window
+        // Initialize a new instance of the popup window
         mPopupWindow = new PopupWindow(
                 customView,
                 LayoutParams.WRAP_CONTENT,
                 LayoutParams.WRAP_CONTENT
         );
 
-        if(Build.VERSION.SDK_INT>=21){
+        if (Build.VERSION.SDK_INT >= 21) {
             mPopupWindow.setElevation(5.0f);
             Log.v(TAG, "Elevation set");
         }
 
 
-        // Get a reference for the custom view elements
+        // Get references for the view elements in the popup (title, picture, blurb, 2 buttons)
         final ImageButton closeButton = (ImageButton) customView.findViewById(R.id.close);
-        final TextView title = (TextView)customView.findViewById(R.id.title);
+        final TextView title = (TextView) customView.findViewById(R.id.title);
         final ImageView picture = (ImageView) customView.findViewById(R.id.picture);
         final TextView blurb = (TextView) customView.findViewById(R.id.blurb);
         final Button directions = (Button) customView.findViewById(R.id.directions);
 
 
-        // Set a click listener for the popup window close button
+        // Set custom click listener for the popup window close button
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -151,50 +138,58 @@ public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClick
             }
         });
 
+        //Set custom click listener for the "Get Directions" button
         directions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass( TourGuide_Roadmap.this, Directions.class);
+                intent.setClass(TourGuide_Roadmap.this, Directions.class);
                 startActivity(intent);
             }
         });
 
+        //Switch for each case for buttons, populates new popup instance with corresponding info
         switch (v.getId()) {
 
             case R.id.button1:
                 title.setText("Tansey Gym");
                 blurb.setText(getString(R.string.tansey_blurb));
                 picture.setImageResource(R.drawable.tansey);
+                Log.v(TAG, "Current Title: " + title.getText());
                 break;
 
             case R.id.button2:
                 title.setText("Wattson Auditorium");
                 blurb.setText(getString(R.string.wattson_blurb));
                 picture.setImageResource(R.drawable.wattson);
+                Log.v(TAG, "Current Title: " + title.getText());
                 break;
 
             case R.id.button3:
                 title.setText("Beatty Hall");
                 blurb.setText(getString(R.string.beatty_blurb));
                 picture.setImageResource(R.drawable.beatty);
+                Log.v(TAG, "Current Title: " + title.getText());
                 break;
             case R.id.button4:
                 title.setText("555 Huntington Ave");
                 blurb.setText(getString(R.string.huntington_blurb));
                 picture.setImageResource(R.drawable.huntington);
+                Log.v(TAG, "Current Title: " + title.getText());
                 break;
 
             case R.id.button5:
                 title.setText("Baker Hall");
                 blurb.setText(getString(R.string.baker_blurb));
                 picture.setImageResource(R.drawable.baker);
+                Log.v(TAG, "Current Title: " + title.getText());
                 break;
 
             case R.id.button6:
                 title.setText("The Leopard Statue");
                 blurb.setText(getString(R.string.leopard_blurb));
                 picture.setImageResource(R.drawable.leopard_statue);
+                Log.v(TAG, "Current Title: " + title.getText());
                 break;
 
             default:
@@ -202,52 +197,68 @@ public class TourGuide_Roadmap extends AppCompatActivity implements View.OnClick
         }
 
         //Show the popup window with title, image, blurb, etc
-        mPopupWindow.showAtLocation(mConstraintLayout, Gravity.CENTER,0,0);
+        mPopupWindow.showAtLocation(mConstraintLayout, Gravity.CENTER, 0, 0);
         Log.v(TAG, "It's here it's here");
     }
 
-    private void readFromIntent(Intent intent) {
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            NdefMessage[] msgs = null;
-            if (rawMsgs != null) {
-                msgs = new NdefMessage[rawMsgs.length];
-                for (int i = 0; i < rawMsgs.length; i++) {
-                    msgs[i] = (NdefMessage) rawMsgs[i];
-                }
-            }
-            buildTagViews(msgs);
-        }
-    }
-    private void buildTagViews(NdefMessage[] msgs) {
-        if (msgs == null || msgs.length == 0) return;
+    public void onNfcDetected(Ndef ndef){
 
-        String text = "";
-//        String tagId = new String(msgs[0].getRecords()[0].getType());
-        byte[] payload = msgs[0].getRecords()[0].getPayload();
-        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
-        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
-        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+        readFromNFC(ndef);
+    }
+
+    private void readFromNFC(Ndef ndef) {
 
         try {
-            // Get the Text
-            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-        } catch (UnsupportedEncodingException e) {
-            Log.e("UnsupportedEncoding", e.toString());
-        }
+            ndef.connect();
+            NdefMessage ndefMessage = ndef.getNdefMessage();
 
-        tvNFCContent.setText("NFC Content: " + text);
+            //String "message" contains the contents of the NFC tag in String form
+            String message = new String(ndefMessage.getRecords()[0].getPayload());
+            Log.d(TAG, "readFromNFC: " + message);
+            ndef.close();
+
+        } catch (IOException | FormatException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    private void initNFC(){
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected,tagDetected,ndefDetected};
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if(mNfcAdapter!= null)
+            mNfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mNfcAdapter!= null)
+            mNfcAdapter.disableForegroundDispatch(this);
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        readFromIntent(intent);
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        Log.d(TAG, "onNewIntent: "+intent.getAction());
+
+        if(tag != null) {
+            Toast.makeText(this, getString(R.string.tag_detected), Toast.LENGTH_SHORT).show();
+            Ndef ndef = Ndef.get(tag);
+            onNfcDetected(ndef);
         }
     }
 }
